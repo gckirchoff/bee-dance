ee36c7ad1891ab9468a95b31f77599ea098d7033
Gregory Kirchoff <gckirchoff@gmail.com>
2022-09-17 15:03:54 -0400

    Added grass

--- src/utils/draw/agents/Field.ts
+++ src/utils/draw/agents/Field.ts
@@ -1,19 +1,58 @@
+import { Vector } from './Vector';
+
 export class Field {
   private w: number;
   private h: number;
 
+  private radius: number;
+  private center: Vector;
+  private grass: Vector[];
+
   constructor(w: number, h: number) {
     this.w = w;
     this.h = h;
+
+    this.radius = w * 1.5;
+    this.center = new Vector(w / 2, h + w * 0.95);
+    this.grass = this.generateGrass(400);
   }
 
   draw = (ctx: CanvasRenderingContext2D) => {
+    const { x, y } = this.center;
     ctx.save();
     ctx.fillStyle = 'green';
-    ctx.translate(this.w / 2, this.h);
+    ctx.translate(x, y);
     ctx.beginPath();
-    ctx.arc(0, this.w * 0.95, this.w * 1.5, 0, Math.PI, true);
+    ctx.arc(0, 0, this.radius, 0, Math.PI, true);
     ctx.fill();
     ctx.restore();
+
+    this.drawGrass(ctx);
+  };
+
+  private generateGrass = (num = 100): Vector[] => {
+    const { x: centerX, y: centerY } = this.center;
+    const grassCords: Vector[] = [];
+    for (let i = 0; i < num; i++) {
+      const r = this.radius * Math.sqrt(Math.random());
+      const theta = Math.random() * 2 * Math.PI;
+      const x = centerX + r * Math.cos(theta);
+      const y = centerY + r * Math.sin(theta);
+      grassCords.push(new Vector(x, y));
+    }
+    return grassCords;
+  };
+
+  private drawGrass = (ctx: CanvasRenderingContext2D) => {
+    this.grass.forEach((bladePosition) => {
+      const { x, y } = bladePosition;
+      const width = 3;
+      const height = 10;
+      ctx.save();
+      ctx.fillStyle = 'darkgreen';
+      ctx.translate(x, y);
+      ctx.fillRect(-width / 2, -height / 2, width, height);
+      ctx.restore();
+    });
   };
 }
