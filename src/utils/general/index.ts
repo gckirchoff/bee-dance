40c36b896bd08029070e5537c833961726660b7a
Gregory Kirchoff <gckirchoff@gmail.com>
2022-09-18 10:54:42 -0400

    Added enum and refactored time getting

--- src/utils/general/index.ts
+++ src/utils/general/index.ts
@@ -1,16 +1,25 @@
+enum Meridiem {
+  AM = 'AM',
+  PM = 'PM',
+}
+
 export const minsToTime = (mins: number): string => {
   const hours = mins / 60;
   const minutes = mins % 60;
   const formattedHours = formatHours(hours);
-  const formattedMinutes = formatMinutes(minutes);
-  const time = `${formattedHours}:${formattedMinutes}`;
-  return time;
+  const formattedMinutes = cleanUpTimeString(minutes);
+  const meridiem = getMeridiem(hours);
+
+  return `${formattedHours}:${formattedMinutes} ${meridiem}`;
 };
 
 const formatHours = (hours: number): string => {
   const timeHour = hours > 13 ? hours - 12 : hours;
-  return Math.trunc(timeHour).toString().padStart(2, '0');
+  return cleanUpTimeString(timeHour);
 };
 
-const formatMinutes = (minutes: number): string =>
-  Math.trunc(minutes).toString().padStart(2, '0');
+const cleanUpTimeString = (time: number): string =>
+  Math.trunc(time).toString().padStart(2, '0');
+
+const getMeridiem = (hours: number): Meridiem =>
+  hours >= 12 ? Meridiem.PM : Meridiem.AM;
