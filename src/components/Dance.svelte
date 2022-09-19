b2cb3ebc0b7d332d05600846d1a0b677d7ac53ea
Gregory Kirchoff <gckirchoff@gmail.com>
2022-09-19 19:06:43 -0400

    Using canvas width reset every tick to clear previous images

--- src/components/Dance.svelte
+++ src/components/Dance.svelte
@@ -1,28 +1,31 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+  import { afterUpdate, onMount } from 'svelte';
   import Bee from '../assets/bee.svg';
   let w: number;
   let h: number;
 
   let canvas: HTMLCanvasElement;
 
+  afterUpdate(() => {
+    console.log('iupdating');
+  });
+
   onMount(() => {
     canvas.width = w;
     canvas.height = h;
     const ctx = canvas.getContext('2d');
+
     const bee = new Image();
     bee.src = Bee;
-    // bee.onload = () => {
-    //   ctx.drawImage(bee, 0, 0);
-    // };
 
     let animationFrame = requestAnimationFrame(animate);
     function animate(t: number) {
       ctx.clearRect(0, 0, canvas.width / 2, canvas.height / 2);
-
+      canvas.width = w;
       ctx.save();
       ctx.translate(w * 0.5, h * 0.5);
       ctx.fillRect(0, 0, 10, 10);
+      ctx.scale(0.5, 0.5);
       ctx.drawImage(bee, -180, -170);
       ctx.restore();
 
