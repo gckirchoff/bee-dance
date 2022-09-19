9bc9cf90b64418198be64dcd5298ff8ebae3c0ff
Gregory Kirchoff <gckirchoff@gmail.com>
2022-09-19 18:49:55 -0400

    Added input styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Canvas from './components/Canvas.svelte';
+  import Dance from './components/Dance.svelte';
   import Field from './components/Field.svelte';
   import Range from './components/Range.svelte';
   import { minsToTime } from './utils/general';
@@ -14,28 +15,32 @@
 <main>
   <section class="upper">
     <div class="canvas-container left"><Field {timeInMins} /></div>
-    <div class="canvas-container right" />
+    <div class="canvas-container right" ><Dance /></div>
   </section>
   <section class="lower">
-    <label for="time-input">Time: {minsToTime(timeInMins)}</label>
-    <Range
-      on:change={updateTime}
-      initialValue={timeInMins}
-      min={360}
-      max={1080}
-      showTooltip={false}
-      id="time-input"
-    />
+    <div class="inputs-container">
+      <div class="time-input-container">
+        <label for="time-input">Time: {minsToTime(timeInMins)}</label>
+        <Range
+          on:change={updateTime}
+          initialValue={timeInMins}
+          min={360}
+          max={1080}
+          showTooltip={false}
+          id="time-input"
+        />
+      </div>
+    </div>
   </section>
 </main>
 
 <style>
   main {
     min-height: 100vh;
+    background-color: rgb(60, 64, 67);
   }
 
   .upper {
-    background-color: red;
     height: 80vh;
     display: flex;
     align-items: stretch;
@@ -46,15 +51,22 @@
     height: 20vh;
   }
 
+  .inputs-container {
+    display: flex;
+  }
+
+  .time-input-container {
+    display: flex;
+    flex: 0 1 50%;
+  }
+
   .canvas-container {
     flex: 1 1 0;
   }
 
   .left {
-    background-color: blue;
   }
 
   .right {
-    background-color: yellow;
   }
 </style>
