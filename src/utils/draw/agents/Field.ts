ba8d9b86d172f6a9b20dc02255af7d494f0473a8
Gregory Kirchoff <gckirchoff@gmail.com>
2022-09-19 08:42:53 -0400

    Refactored colors to be class properties and added two methods for seeing if vector position is in field and to draw a flower

--- src/utils/draw/agents/Field.ts
+++ src/utils/draw/agents/Field.ts
@@ -1,3 +1,4 @@
+import { Flower } from './Flower';
 import { Vector } from './Vector';
 
 export class Field {
@@ -7,6 +8,9 @@ export class Field {
   private radius: number;
   private center: Vector;
   private grass: Vector[];
+
+  private fieldColor = '#228C22';
+  private grassColor = 'green';
 
   constructor(w: number, h: number) {
     this.w = w;
@@ -20,7 +24,7 @@ export class Field {
   draw = (ctx: CanvasRenderingContext2D) => {
     const { x, y } = this.center;
     ctx.save();
-    ctx.fillStyle = 'green';
+    ctx.fillStyle = this.fieldColor;
     ctx.translate(x, y);
     ctx.beginPath();
     ctx.arc(0, 0, this.radius, 0, Math.PI, true);
@@ -31,6 +35,15 @@ export class Field {
   };
 
   getCenterOfEarth = (): Vector => this.center;
+
+  isInField = ({ x, y }: Vector) =>
+    Math.pow(x - this.center.x, 2) + Math.pow(y - this.center.y, 2) <
+    Math.pow(this.radius, 2);
+
+  drawFlower = (flowerLocation: Vector, ctx: CanvasRenderingContext2D) => {
+    const flower = new Flower(flowerLocation, this.w, this.h);
+    flower.draw(ctx);
+  };
 
   private generateGrass = (num = 100): Vector[] => {
     const { x: centerX, y: centerY } = this.center;
@@ -51,7 +64,7 @@ export class Field {
       const width = 3;
       const height = 10;
       ctx.save();
-      ctx.fillStyle = 'darkgreen';
+      ctx.fillStyle = this.grassColor;
       ctx.translate(x, y);
       ctx.fillRect(-width / 2, -height / 2, width, height);
       ctx.restore();
